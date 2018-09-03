fb23966f3b4ffd451ca96452fef95dd875d68021
bob <301085344@qq.com>
2018-09-03 17:56:21 +0800

    测试编辑器功能

---
+++ src/pages/edit/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="edit-page">
+		<div class="pub" @click="ispub=!ispub" v-show="ispub">发布</div>
+		<mavon-editor v-if="ispub" class="edit" :ishljs = "true" @change="change"></mavon-editor>
+		<div class="view markdown-body" v-html="value" v-else>
+			
+		</div>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+				value:"",
+				ispub:true
+			}
+		},
+		methods:{
+			change(value, render){
+				this.value = render;
+			}
+		}
+	}
+</script>
+<style lang="scss">
+	.edit-page{
+		.pub{
+			background-color: #007fff;
+			border-radius: 3px;
+			width: 80px;
+			height: 30px;
+			line-height: 30px;
+			color: #fff;
+			text-align: center;
+		}
+		.edit{
+			height: 800px;
+		}
+		.view{
+			width: 700px;
+			margin: 0 auto;
+		}
+	}
+</style>
